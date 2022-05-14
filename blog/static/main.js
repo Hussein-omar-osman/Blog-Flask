a4d13958d36f27e8c9b6796b04e6b69d5fad01c4
Hussein Omar <husseinomar6190@gmail.com>
2022-05-14 20:30:57 +0300

    adding a the quote of the day fearture

--- blog/static/main.js
+++ blog/static/main.js
@@ -1,7 +1,6 @@
 const btnJoke = document.getElementById('joke');
-const p1 = document.getElementById('p1');
-const p2 = document.getElementById('p2');
-const p3 = document.getElementById('p3');
+const p2 = document.getElementById('quote');
+const p3 = document.getElementById('author');
 
 btnJoke.addEventListener('click', () => {
   getData();
@@ -11,15 +10,13 @@ async function getData() {
   let res = await fetch('http://quotes.stormconsultancy.co.uk/random.json');
   let data = await res.json();
   if (data) {
-    // console.log(data.quote);
+    console.log(data);
+    p2.innerText = data.quote;
+    p3.innerText = `- ${data.author} -`;
+    return data;
   } else {
     console.log('no data');
   }
-
-  p1.innerText = data.id;
-  p2.innerText = data.quote;
-  p3.innerText = data.author;
-  return data;
 }
 
 let obj = {
