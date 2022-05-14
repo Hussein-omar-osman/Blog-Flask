79d93f69f9d2ae7d3776abf5fb963d24a04fbdaa
Hussein Omar <husseinomar6190@gmail.com>
2022-05-14 22:12:09 +0300

    creating a route for login and register templates

--- blog/views.py
+++ blog/views.py
@@ -4,3 +4,11 @@ from blog import app
 @app.route('/')
 def home():
  return render_template('home.html', title='Home')
+
+@app.route("/register", methods=['GET', 'POST'])
+def register():
+ return render_template('register.html', title='Register')
+
+@app.route("/login", methods=['GET', 'POST'])
+def login():
+ return render_template('login.html', title='Login')
