243e2ea1bd992ae802e52f36ec9fde7e88014b4c
Hussein Omar <husseinomar6190@gmail.com>
2022-05-15 10:15:29 +0300

    adding users to the database

--- blog/forms.py
+++ blog/forms.py
@@ -1,6 +1,8 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, SubmitField, TextAreaField, SelectField, BooleanField
 from wtforms.validators import DataRequired, Length, Email, EqualTo, ValidationError
+from blog.models import User
+
 
 class RegistrationForm(FlaskForm):
     username = StringField('Username',
@@ -11,6 +13,18 @@ class RegistrationForm(FlaskForm):
     confirm_password = PasswordField('Confirm Password',
                                      validators=[DataRequired(), EqualTo('password')])
     submit = SubmitField('Sign Up')
+    
+    def validate_username(self, username):
+        
+        user = User.query.filter_by(username=username.data).first()
+        if user:
+            raise ValidationError('Username is taken')
+        
+    def validate_email(self, email):
+        
+        user = User.query.filter_by(email=email.data).first()
+        if user:
+            raise ValidationError('Email is taken')
     
     
 class LoginForm(FlaskForm):
--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,8 @@
-from flask import Flask, render_template, url_for, flash, redirect, request
-from blog import app
+from flask import render_template, url_for, flash, redirect, request
+from blog import app, db, bc
 from blog.forms import LoginForm, RegistrationForm
+from blog.models import User, Post, Comments
+
 @app.route('/')
 def home():
  return render_template('home.html', title='Home')
@@ -8,15 +10,25 @@ def home():
 @app.route("/register", methods=['GET', 'POST'])
 def register():
  form = RegistrationForm()
+ if form.validate_on_submit():
+    hashed_password = bc.generate_password_hash(form.password.data).decode('utf-8')
+    user = User(username=form.username.data, email=form.email.data, password=hashed_password)
+    db.session.add(user)
+    db.session.commit()
+    flash(f'Account created for {form.username.data}! You can Log in any time', 'secondary')
+    return redirect(url_for('login'))
  return render_template('register.html', title='Register', form=form)
 
 @app.route("/login", methods=['GET', 'POST'])
 def login():
  form = LoginForm()
  if form.validate_on_submit():
-  if form.email.data == 'ali@gmail.com' and form.password.data == '123':
-   flash('login successful', 'secondary')
-   return redirect(url_for('home'))
+  print(form.email.data)
+  user = User.query.filter_by(email=form.email.data).first()
+  if user and bc.check_password_hash(user.password, form.password.data):
+      flash(f'{user.username}, You have been logged in!', 'primary')
+      next_page = request.args.get('next')
+      return redirect(next_page) if next_page else redirect(url_for('home'))
   else:
-   flash('login unsuccessful', 'danger')
+      flash('Login Unsuccessful. Please check email and password', 'danger')
  return render_template('login.html', title='Login', form=form)
