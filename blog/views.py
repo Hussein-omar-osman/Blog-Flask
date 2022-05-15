407d8e2456a346d0d05a325f31d98284dcb6339e
Hussein Omar <husseinomar6190@gmail.com>
2022-05-15 10:36:56 +0300

    allowing users to login and logout

--- blog/views.py
+++ blog/views.py
@@ -2,6 +2,8 @@ from flask import render_template, url_for, flash, redirect, request
 from blog import app, db, bc
 from blog.forms import LoginForm, RegistrationForm
 from blog.models import User, Post, Comments
+from flask_login import login_user, current_user, logout_user, login_required
+
 
 @app.route('/')
 def home():
@@ -9,6 +11,8 @@ def home():
 
 @app.route("/register", methods=['GET', 'POST'])
 def register():
+ if current_user.is_authenticated:
+   return redirect(url_for('home'))
  form = RegistrationForm()
  if form.validate_on_submit():
     hashed_password = bc.generate_password_hash(form.password.data).decode('utf-8')
@@ -21,14 +25,24 @@ def register():
 
 @app.route("/login", methods=['GET', 'POST'])
 def login():
+ if current_user.is_authenticated:
+   return redirect(url_for('home'))
  form = LoginForm()
+ 
  if form.validate_on_submit():
-  print(form.email.data)
   user = User.query.filter_by(email=form.email.data).first()
   if user and bc.check_password_hash(user.password, form.password.data):
-      flash(f'{user.username}, You have been logged in!', 'primary')
+      login_user(user)
+      flash(f'{user.username}, You have been logged in!', 'secondary')
       next_page = request.args.get('next')
       return redirect(next_page) if next_page else redirect(url_for('home'))
   else:
       flash('Login Unsuccessful. Please check email and password', 'danger')
  return render_template('login.html', title='Login', form=form)
+
+
+@app.route("/logout")
+def logout():
+    logout_user()
+    flash('You have successfully logged out', 'secondary')
+    return redirect(url_for('home'))
